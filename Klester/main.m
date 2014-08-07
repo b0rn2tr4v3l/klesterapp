681e2f4984d9191dc3db36246fdc5169a72d7324
Stefan Brankovic <stefan@meshunlimited.com>
2014-08-07 09:29:54 -0700

    Initial Commit

---
+++ Klester/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Klester
+//
+//  Created by Stefan Brankovic on 8/7/14.
+//  Copyright (c) 2014 Mesh Unlimited, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MUAppDelegate class]));
+    }
+}
---
+++ KlesterTests/KlesterTests.m
@@ -0,0 +1,34 @@
+//
+//  KlesterTests.m
+//  KlesterTests
+//
+//  Created by Stefan Brankovic on 8/7/14.
+//  Copyright (c) 2014 Mesh Unlimited, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KlesterTests : XCTestCase
+
+@end
+
+@implementation KlesterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
